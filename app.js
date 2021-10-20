ae1d3f8f8fc37997f40540f878a2dc4f28c59894
Micah Sterling <msterling89@gmail.com>
2021-10-20 15:33:48 -0500

    add get req for index and id

--- app.js
+++ app.js
@@ -2,15 +2,30 @@ const express = require('express');
 const app = express();
 const EventEmitter = require('events');
 
+const users = [
+  {id:1, name: 'Jack', points: 1000},
+  {id:2, name: 'James',points: 2000},
+  {id:3, name: 'Jim', points: 3000},
+];
+
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('hello world!!!');
 });
 
 app.get('/api/users', (rep,res) => {
-  res.send([1,2,3]);
+  res.send(users);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.get('/api/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) res.status(404).send('Users Id was not found');
+  res.send(user);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
 
 const User = require('./user');
 const user = new User();
